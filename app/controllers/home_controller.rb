991c2f9787adea98dbd511ba1df72d1a82f03017
yashm <yshmarov@gmail.com>
2018-06-02 17:38:04 +0000

    rails g controller home index pricing features contact_us privacy_policy terms_of_service customer_agreement security

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+  def index;   end
+
+  def pricing;  end
+
+  def features;  end
+
+  def contact_us;  end
+
+  def privacy_policy;  end
+
+  def terms_of_service;  end
+
+  def customer_agreement;  end
+
+  def security;  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  get 'index', to: 'home#index'
+  get 'pricing', to: 'home#pricing'
+  get 'features', to: 'home#features'
+  get 'contact_us', to: 'home#contact_us'
+  get 'privacy_policy', to: 'home#privacy_policy'
+  get 'terms_of_service', to: 'home#terms_of_service'
+  get 'customer_agreement', to: 'home#customer_agreement'
+  get 'security', to: 'home#security'
+
   get 'hello_world', to: 'hello_world#index'
 
-  root to: 'landing_page#index'
 end
