2261e9d558ab064a44c7bb67ebfd182094450cb1
Punkbooster <ugliar.arsen@gmail.com>
2018-06-06 13:23:59 +0200

    Add user/event model tests

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Event, type: :model do
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:starts_at) }
+    it { is_expected.to validate_presence_of(:ends_at) }
+    it { is_expected.to validate_presence_of(:user) }
+
+    it { should validate_length_of(:title).is_at_most(100) }
+    it { should validate_length_of(:description).is_at_most(100) }
+  end
+
+  context 'associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+end
