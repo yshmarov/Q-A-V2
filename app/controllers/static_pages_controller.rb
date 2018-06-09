af75391ffa3b08ec542050b1d86399423438fdd6
yashm <yshmarov@gmail.com>
2018-06-09 16:26:14 +0000

    rename home_controller to static_pages_controller everywhere

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class StaticPagesController < ApplicationController
   def pricing; end
 
   def features; end
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,14 @@ Rails.application.routes.draw do
   root to: 'landing_page#index'
 
   # Static pages
-  get 'pricing', to: 'home#pricing'
-  get 'features', to: 'home#features'
-  get 'contact_us', to: 'home#contact_us'
-  get 'privacy_policy', to: 'home#privacy_policy'
-  get 'terms_of_service', to: 'home#terms_of_service'
-  get 'customer_agreement', to: 'home#customer_agreement'
-  get 'security', to: 'home#security'
-  get 'jobs', to: 'home#jobs'
+  get 'pricing', to: 'static_pages#pricing'
+  get 'features', to: 'static_pages#features'
+  get 'contact_us', to: 'static_pages#contact_us'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  get 'terms_of_service', to: 'static_pages#terms_of_service'
+  get 'customer_agreement', to: 'static_pages#customer_agreement'
+  get 'security', to: 'static_pages#security'
+  get 'jobs', to: 'static_pages#jobs'
 
   get 'hello_world', to: 'hello_world#index'
 end
