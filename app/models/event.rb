3b7e7427507c30c2331ed1635d8ffe28329e1b8d
yashm <yshmarov@gmail.com>
2018-06-18 18:24:40 +0000

    hide event.description, add event.starts_at minute_step: 15

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :questions
 
-  validates :title, :starts_at, :ends_at, :user, presence: true
+  validates :title, :event_code, :starts_at, :ends_at, :user, presence: true
   validates :title, :description, length: { maximum: 100 }
+  validates :event_code, uniqueness: true
 end
