aad7eddbe947d4d83519fd7a611163c02593516d
yashm <yshmarov@gmail.com>
2018-06-20 22:05:29 +0000

    ends_at_can_not_be_before_starts_at

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,8 @@ class Event < ApplicationRecord
   validates :title, :password, :starts_at, :ends_at, :user, presence: true
   validates :title, :description, length: { maximum: 100 }
   validates :password, uniqueness: true
-  validate :starts_at_cannot_be_in_the_past, on: :create
+  validate :starts_at_cannot_be_in_the_past
+  validate :ends_at_can_not_be_before_starts_at
 
   default_scope { order(created_at: :desc) }
   
@@ -25,7 +26,13 @@ class Event < ApplicationRecord
 
   def starts_at_cannot_be_in_the_past
     if starts_at.present? && starts_at <= Time.now
-      errors.add(:starts_at, "The fundamental laws of nature prevent time travel")
+      errors.add(:starts_at, "- You can not start an event in the past")
+    end
+  end
+
+  def ends_at_can_not_be_before_starts_at
+    if ends_at.present? && ends_at <= starts_at
+      errors.add(:ends_at, "- Event can not end before it starts")
     end
   end
 
