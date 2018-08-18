3e13a6628c656dc985e91ca39732eb4ef8cbfa90
Punkbooster <ugliar.arsen@gmail.com>
2018-08-18 16:58:14 +0200

    Create session for not logged users

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -3,16 +3,18 @@ module Api
 
     def create
       question = Question.create!(question_params)
+      assign_user_or_session(question)
 
       json_response(serialized_questions(question.event), :created)
     end
 
     def vote
       question = Question.find_by(id: question_params[:id])
+      voter = current_user || find_or_create_session
 
-      if current_user.voted_for? question
+      if voter.voted_for? question
         json_response({ error: 'Already voted.' }, :unprocessable_entity)
-      elsif question.upvote_by current_user
+      elsif question.upvote_by voter
         json_response(serialized_questions(question.event), :created)
       else
         json_response({ error: 'Something went wrong.' }, :unprocessable_entity)
@@ -20,6 +22,18 @@ module Api
     end
 
     private
+
+    def assign_user_or_session(question)
+      if current_user
+        question.update!(user: current_user)
+      else
+        question.update!(session: find_or_create_session)
+      end
+    end
+
+    def find_or_create_session
+      Session.find_or_create_by!(ip: request.remote_ip)
+    end
 
     def serialized_questions(event)
       event.questions.map do |q|
